ae071e9f1b74a25d7c3132ec34f10e10ee674181
gokulvasanthJ <gokul27mech@gmail.com>
2023-04-22 12:52:46 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,9 @@
+var element=document.createElement("table")
+var ele=document.createElement("tr")
+var ele1=document.createElement("th")
+var ele2=document.createElement("td")
+
+ele1.append(ele2)
+ele.append(ele1)
+element.append(ele)
+document.body.append(element)
